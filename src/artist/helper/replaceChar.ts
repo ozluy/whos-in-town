7d343f1c5f81dcc2c15b2fb81b6acda7ff441318
Yusuf Ozlu <yusuf_ozlu@epam.com>
2023-02-27 08:58:15 +0100

    optimize function

--- src/artist/helper/replaceChar.ts
+++ src/artist/helper/replaceChar.ts
@@ -1,8 +1,21 @@
+const charMap: { [k: string]: string } = {
+  "/": "%252F",
+  "?": "%253F",
+  "*": "%252A",
+  '"': "%27C",
+};
+
 const replaceChar = (text: string) => {
-  let newText = text.replace("/", "%252F");
-  newText = newText.replace("?", "%253F");
-  newText = newText.replace("*", "%252A");
-  newText = newText.replace('"', "%27C");
+  let newText = "";
+  for (let i = 0; i < text.length; i++) {
+    const char = text[i];
+    const charToReplace = charMap[char];
+    if (charToReplace) {
+      newText += charToReplace;
+    } else {
+      newText += char;
+    }
+  }
 
   return newText;
 };
